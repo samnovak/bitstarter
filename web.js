82eae55953da8c404c3787d884dc2fe8c4bc9a52
samnovak <samnovak@gmail.com>
2013-07-10 03:15:46 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -10,3 +10,7 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var fs = require('fs');
+var buf = fs.readFileSync(index.html);
+buf.toString();
