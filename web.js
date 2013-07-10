2d4db906fe26b04dabfb8a6e9a6a8b5559f36819
samnovak <samnovak@gmail.com>
2013-07-10 04:15:34 +0000

    Debugging

--- web.js
+++ web.js
@@ -4,10 +4,9 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 var teststring = "Hello World";
 var buffer = new Buffer(256);
-buffer.write(fs.readFileSync('~/index.html'));
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf8'));
+  response.send(buffer.write("Hello", "utf-8"));
 });
 
 var port = process.env.PORT || 5000;
