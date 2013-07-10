109765935eda482e7a9f4179c23d0a48d8ee9908
samnovak <samnovak@gmail.com>
2013-07-10 03:26:07 +0000

    Debugging

--- web.js
+++ web.js
@@ -2,10 +2,6 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var fs = require('fs');
-var buf = fs.readFileSync(index.html);
-var stringout = buf.toString();
-
 app.get('/', function(request, response) {
   response.send("Hello World!");
 });
