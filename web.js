b51b06d178b73cf2d754f4242aff8165010670e3
samnovak <samnovak@gmail.com>
2013-07-10 03:37:50 +0000

    Debugging

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var fs = require('fs');
+
 var teststring = "Hello World";
 
 app.get('/', function(request, response) {
@@ -12,3 +13,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+fs.readFileSync('index.html');
