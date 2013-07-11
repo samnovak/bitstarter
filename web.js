2b0ee0c29b6c9bc5725c735514f10e85b1b6ee37
samnovak <samnovak@gmail.com>
2013-07-11 00:45:26 +0000

    Debugging

--- web.js
+++ web.js
@@ -5,7 +5,10 @@ var fs = require('fs');
 var teststring = "Hello World";
 var buffer = new Buffer(256);
 buffer.write("hello!","utf-8");
-
+fs.readFile('/etc/passwd', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
 app.get('/', function(request, response) {
   response.send(buffer.toString('utf-8',0,6));
 });
