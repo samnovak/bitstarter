f3ba2513843ee2d5ecdcf833b981a1cc3561f482
samnovak <samnovak@gmail.com>
2013-07-11 01:01:49 +0000

    Debugging

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
-
-
+// way to do it without a buffer since fs.readFileSync
+// says if you include encoding option ('utf-8') in 
+// this case then this function returns a string
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var content = fs.readFileSync('index.html','utf-8);
+  var content = fs.readFileSync('index.html', 'utf-8');
   response.send(content);
 });
 
