95806b0f6abcd1243116082d405ae882493b7228
samnovak <samnovak@gmail.com>
2013-07-11 00:59:39 +0000

    Debugging

--- web.js
+++ web.js
@@ -1,16 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var fs = require('fs');
-var teststring = "Hello World";
-var buffer = new Buffer(256);
-buffer.write("","utf-8");
-fs.readFile("/home/ubuntu/bitstarter/index.html', function (err, data) {
-  if (err) throw err;
-  console.log(buffer);
-});
+
+
+
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8',0,6));
+  var fs = require('fs');
+  var content = fs.readFileSync('index.html','utf-8);
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
